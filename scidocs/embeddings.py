3cac66e4b2f6d64c057531fbac1ac3f8195ceddf
Ronald Seoh <ronaldseoh@icloud.com>
2021-07-30 13:52:55 -0400

    scidocs/embeddings.py: add a simple pytorch NN module

--- scidocs/embeddings.py
+++ scidocs/embeddings.py
@@ -1,6 +1,59 @@
 import ujson as json
 import numpy as np
+import torch
 from tqdm import tqdm
+
+
+class SimpleNet(torch.nn.Module):
+
+    def __init__(self, input_dim, output_dim, hidden_dim, n_hidden, dropout_rate):
+        super(SimpleNet, self).__init__()
+
+        # Setup layers
+        # Input layer
+        self.input = torch.nn.ModuleDict({
+            'linear': torch.nn.Linear(input_dim, hidden_dim),
+            'dropout': torch.nn.Dropout(dropout_rate),
+            'nonlinear': torch.nn.ReLU(),
+        })
+
+        # Hidden Layer(s)
+        if n_hidden > 0:
+            self.hidden_layers = torch.nn.ModuleList()
+
+            for i in range(n_hidden):
+                self.hidden_layers.append(
+                    torch.nn.ModuleDict({
+                        'linear': torch.nn.Linear(hidden_dim, hidden_dim),
+                        'dropout': torch.nn.Dropout(dropout_rate),
+                        'nonlinear': torch.nn.ReLU(),
+                    })
+                )
+
+        # Output
+        self.output = torch.nn.ModuleDict({
+            'linear': torch.nn.Linear(hidden_dim, output_dim),
+            'dropout': torch.nn.Dropout(dropout_rate),
+        })
+
+    def forward(self, X):
+        # Forward through the input layer
+        activation = self.input['linear'](X)
+        activation = self.input['dropout'](activation)
+        activation = self.input['nonlinear'](activation)
+
+        # Forward through hidden layers
+        if hasattr(self, 'hidden_layers'):
+            for hidden in self.hidden_layers:
+                activation = hidden['linear'](activation)
+                activation = hidden['dropout'](activation)
+                activation = hidden['nonlinear'](activation)
+
+        activation = self.output['linear'](activation)
+        activation = self.output['dropout'](activation)
+
+        return activation
+
 
 def load_embeddings_from_jsonl(embeddings_path):
     """Load embeddings from a jsonl file.
