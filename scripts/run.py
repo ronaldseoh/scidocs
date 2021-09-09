818db3e4eb88a63e80e9e0d243f9d08faa56c161
Ronald Seoh <ronaldseoh@icloud.com>
2021-09-09 15:28:47 -0400

    scripts/run.py: save the dataframe with scores to an excel file

--- scripts/run.py
+++ scripts/run.py
@@ -22,6 +22,9 @@ def main():
     parser.add_argument('--n-jobs', default=12, help='number of parallel jobs for classification (related to mesh/mag metrics)', type=int)
     parser.add_argument('--cuda-device', default=-1, help='specify if you want to use gpu for training the recommendation model; -1 means use cpu')
     parser.add_argument('--data-path', default=None, help='path to the data directory where scidocs files reside. If None, it will default to the `data/` directory')
+
+    parser.add_argument('--results-save-path', default=None, help='path to the directory to save the results')
+
     args = parser.parse_args()
 
     data_paths = DataPaths(args.data_path)
@@ -47,7 +50,10 @@ def main():
                 key = k + '-' + kk
                 flat_metrics[key] = vv
     df = pd.DataFrame(list(flat_metrics.items())).T
+
     print(df)
+
+    df.to_excel(args.results_save_path)
 
 if __name__ == '__main__':
     main()
