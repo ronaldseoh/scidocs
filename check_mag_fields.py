c74b5a60e40c931880c9f51fbfea5db84c44a284
Ronald Seoh <ronaldseoh@icloud.com>
2022-05-08 16:33:34 -0400

    Update check_mag_fields.py

--- check_mag_fields.py
+++ check_mag_fields.py
@@ -33,28 +33,45 @@ if __name__ == '__main__':
             query_paper_id = l_parsed[0]
             pos_paper_id = l_parsed[2]
 
-            unique_paper_ids.add(query_paper_id)
-            unique_paper_ids.add(pos_paper_id)
-
-            if query_paper_id in extra_metadata.keys() and len(extra_metadata[query_paper_id]) > 0:
-                num_query_paper_ids_found_mag += 1
-
-                for f in extra_metadata[query_paper_id]:
-                    query_paper_count_by_mag_field[f] += 1
-            else:
-                query_paper_count_by_mag_field['**Unknown**'] += 1
-
-            if pos_paper_id in extra_metadata.keys() and len(extra_metadata[pos_paper_id]) > 0:
-                num_pos_paper_ids_found_mag += 1
+            if int(l_parsed[3]) == 1: # positive examples only
+                unique_paper_ids.add(query_paper_id)
+                unique_paper_ids.add(pos_paper_id)
 
                 if query_paper_id in extra_metadata.keys() and len(extra_metadata[query_paper_id]) > 0:
-                    if len(set(extra_metadata[query_paper_id]).intersection(set(extra_metadata[pos_paper_id]))) == 0:
-                        num_triples_pos_cross_domain_pure += 1
+                    num_query_paper_ids_found_mag += 1
 
-                    if len(set(extra_metadata[pos_paper_id]) - set(extra_metadata[query_paper_id])) > 0:
-                        num_triples_pos_cross_domain += 1
+                    for f in extra_metadata[query_paper_id]:
+                        query_paper_count_by_mag_field[f] += 1
+                else:
+                    query_paper_count_by_mag_field['**Unknown**'] += 1
 
-                for f in extra_metadata[pos_paper_id]:
-                    pos_paper_count_by_mag_field[f] += 1
-            else:
-                pos_paper_count_by_mag_field['**Unknown**'] += 1
+                if pos_paper_id in extra_metadata.keys() and len(extra_metadata[pos_paper_id]) > 0:
+                    num_pos_paper_ids_found_mag += 1
+
+                    if query_paper_id in extra_metadata.keys() and len(extra_metadata[query_paper_id]) > 0:
+                        if len(set(extra_metadata[query_paper_id]).intersection(set(extra_metadata[pos_paper_id]))) == 0:
+                            num_triples_pos_cross_domain_pure += 1
+
+                        if len(set(extra_metadata[pos_paper_id]) - set(extra_metadata[query_paper_id])) > 0:
+                            num_triples_pos_cross_domain += 1
+
+                    for f in extra_metadata[pos_paper_id]:
+                        pos_paper_count_by_mag_field[f] += 1
+                else:
+                    pos_paper_count_by_mag_field['**Unknown**'] += 1
+
+    print('num_query_paper_ids_found_mag=', str(num_query_paper_ids_found_mag))
+    print('num_pos_paper_ids_found_mag=', str(num_pos_paper_ids_found_mag))
+    print()
+
+    print('query_paper_count_by_mag_field')
+    print(query_paper_count_by_mag_field)
+    print()
+
+    print('pos_paper_count_by_mag_field')
+    print(pos_paper_count_by_mag_field)
+    print()
+
+    print('num_triples_pos_cross_domain_pure=', str(num_triples_pos_cross_domain_pure))
+    print('num_triples_pos_cross_domain=', str(num_triples_pos_cross_domain))
+    print()
